8dbac5ae65f929e2b4976561cc92dc454761a579
nikolassco <scnoliveira59@gmail.com>
2023-08-09 18:03:15 -0300

    updated dump sql

--- dump.sql
+++ dump.sql
@@ -2,7 +2,7 @@ create database blog;
 
 create table users (
 	id serial primary key,
-  	nome text not null,
+  	name text not null,
   	email text not null unique,
-  	senha text not null
+  	password text not null
 );
